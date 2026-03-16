4432ce01b08a01168a33e4a0b145a96746af16c9
mayoshmase-dot <mayoshmase@gmail.com>
2026-03-17 00:45:09 +0200

    fix

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,7 +2,7 @@ import Banner from '../../components/banner/Banner'
 import Categories from '../../components/categories/Categories'
 import Discounts from '../../components/discounts/Discounts'
 import FeaturedSection from '../../components/featuredSection/FeaturedSection'
-import Hero from '../../components/Hero/Hero'
+import Hero from '../../components/hero/Hero'
 import Products from '../../components/products/Products'
 
 export default function Home() {
