6d96fed43833277305fa38751bc849e241d08650
mayoshmase-dot <mayoshmase@gmail.com>
2026-04-12 21:52:59 +0300

    a

--- src/hooks/useProducts.jsx
+++ src/hooks/useProducts.jsx
@@ -2,13 +2,13 @@ import axiosInstance from '../api/axiosInstance'
 import { useQuery } from '@tanstack/react-query';
 import i18n from '../i18n';
 
-export default function useProducts({ limit = 3, sortBy, price, search, ascending }) {
+export default function useProducts({ limit = 3, sortBy, minPrice, maxPrice, search, ascending }) {
     const getProducts = async () => {
-        const response = await axiosInstance.get(`/Products?limit=${limit}&sortBy=${sortBy}&price=${price},&search=${search},&ascending=${ascending}`);
+        const response = await axiosInstance.get(`/Products?limit=${limit}&sortBy=${sortBy}&minPrice=${minPrice}&maxPrice=${maxPrice}&search=${search}&ascending=${ascending}`);
         return response.data;
     }
     const query = useQuery({
-        queryKey: ['products', i18n.language, limit, sortBy, price, search, ascending],
+        queryKey: ['products', i18n.language, limit, sortBy, minPrice, maxPrice, search, ascending],
         queryFn: getProducts,
     })
     return query
--- src/pages/products/ProductPage.jsx
+++ src/pages/products/ProductPage.jsx
@@ -1,30 +1,41 @@
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
+import useProducts from "../../hooks/useProducts";
+import Loader from "../../ui/loader/Loader";
+import { Box, Button, Container, Grid, Link, MenuItem, Pagination, Select, TextField, Typography } from "@mui/material";
+import Products from "../../ui/products/Products";
+import { Link as ReactLink } from "react-router-dom";
+
 export default function ProductPage() {
     const { t } = useTranslation();
     const [search, setSearch] = useState('')
+    const [page, setPage] = useState("")
     const [minPrice, setMinPrice] = useState('')
     const [maxPrice, setMaxPrice] = useState('')
     const [sortBy, setSortBy] = useState('Price')
     const [ascending, setAscending] = useState(false)
+    const [appliedFilters, setAppliedFilters] = useState({ search: '', minPrice: '', maxPrice: '', sortBy: 'Price', ascending: false })
 
-    const { data, isError, isLoading, error } = useProducts({ limit: 5, sortBy, price: minPrice, search, ascending })
-
+    const { data, isError, isLoading, error } = useProducts({ limit: 5, appliedFilters, page })
+console.log(data)
     if (isLoading) return <Loader />
     if (isError) return <Box color={'red'}>{error.message}</Box>
+
     return (
         <Container maxWidth={'lg'}>
             <Box py={5}>
-                <Typography component={'h2'} variant='h5' mb={3} fontWeight={'bold'}>
+                <Typography component={'h2'} variant='h5' mb={5} fontWeight={'bold'}>
                     {t('Products')}
                 </Typography>
-                <Grid container spacing={3}>
+                <Grid container spacing={5}>
 
-                    <Grid item size={{ xs: 12, md: 3 }}>
-                        <Box sx={{ p: 3, borderRadius: 3, backgroundColor: 'secondary.main', display: 'flex', flexDirection: 'column', gap: 2.5 }}>
+                    <Grid size={{ xs: 12, md: 3 }}>
+                        <Box sx={{ borderRadius: 3, display: 'flex', flexDirection: 'column', gap: 2.5 }}>
                             <Typography fontWeight={'bold'} variant='h6'>{t('Filters')}</Typography>
 
                             <Box>
                                 <Typography variant='body2' fontWeight={'bold'} mb={1}>{t('Search Products')}</Typography>
-                                <TextField fullWidth size='small' placeholder={t('What are you looking for?')}
+                                <TextField fullWidth size='small' placeholder={t('Search Products...')}
                                     value={search} onChange={(e) => setSearch(e.target.value)}
                                 />
                             </Box>
@@ -32,9 +43,9 @@ export default function ProductPage() {
                             <Box>
                                 <Typography variant='body2' fontWeight={'bold'} mb={1}>{t('Price Range')}</Typography>
                                 <Box display={'flex'} gap={1}>
-                                    <TextField size='small' placeholder={t('Min')} value={minPrice}
+                                    <TextField size='small' placeholder={t('Min Price')} value={minPrice}
                                         onChange={(e) => setMinPrice(e.target.value)} type='number' />
-                                    <TextField size='small' placeholder={t('Max')} value={maxPrice}
+                                    <TextField size='small' placeholder={t('Max Price')} value={maxPrice}
                                         onChange={(e) => setMaxPrice(e.target.value)} type='number' />
                                 </Box>
                             </Box>
@@ -51,23 +62,35 @@ export default function ProductPage() {
                             <Box>
                                 <Typography variant='body2' fontWeight={'bold'} mb={1}>{t('Order')}</Typography>
                                 <Select fullWidth size='small' value={ascending} onChange={(e) => setAscending(e.target.value)}>
-                                    <MenuItem value={true}>{t('Descending')}</MenuItem>
-                                    <MenuItem value={false}>{t('Ascending')}</MenuItem>
+                                    <MenuItem value={false}>{t('Descending')}</MenuItem>
+                                    <MenuItem value={true}>{t('Ascending')}</MenuItem>
                                 </Select>
                             </Box>
+
+                            <Button variant="contained" fullWidth onClick={() => { setAppliedFilters({ search, minPrice, maxPrice, sortBy, ascending }) }}>
+                                {t('Apply Filters')}
+                            </Button>
+
                         </Box>
                     </Grid>
 
-                    <Grid item size={{ xs: 12, md: 9 }}>
+                    <Grid size={{ xs: 12, md: 9 }}>
                         <Grid container spacing={3}>
                             {data.response.data.map((product) =>
-                                <Grid item size={{ xs: 12, sm: 6, lg: 4 }} key={product.id}>
+                                <Grid size={{ xs: 12, sm: 6, lg: 4 }} key={product.id}>
                                     <Link component={ReactLink} to={`/product/${product.id}`} underline='none'>
                                         <Products product={product} />
                                     </Link>
                                 </Grid>
                             )}
                         </Grid>
+
+                        <Box display={'flex'} justifyContent={'center'} mt={4}>
+                            <Pagination shape="rounded" count={page}
+                                onChange={(e, value) => setPage(value)}
+                                color="primary"
+                            />
+                        </Box>
                     </Grid>
 
                 </Grid>
